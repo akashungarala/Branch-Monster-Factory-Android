45da3248eb89c01974a6d2e739ec006756140bb9
Akash Ungarala <aungarala@imvreviews.com>
2018-08-27 12:53:35 -0700

    Pushing the code
    Pushing the code

---
+++ Branchster-Android-No-SDK/app/src/main/java/io/branch/branchster/MonsterViewerActivity.java
@@ -0,0 +1,240 @@
+package io.branch.branchster;
+
+import android.app.AlertDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Map;
+
+import io.branch.branchster.fragment.InfoFragment;
+import io.branch.branchster.util.MonsterImageView;
+import io.branch.branchster.util.MonsterObject;
+import io.branch.branchster.util.MonsterPreferences;
+import io.branch.indexing.BranchUniversalObject;
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+import io.branch.referral.util.BranchEvent;
+import io.branch.referral.util.ContentMetadata;
+import io.branch.referral.util.LinkProperties;
+
+public class MonsterViewerActivity extends FragmentActivity implements InfoFragment.OnFragmentInteractionListener {
+    static final int SEND_SMS = 12345;
+
+    private static String TAG = MonsterViewerActivity.class.getSimpleName();
+    public static final String MY_MONSTER_OBJ_KEY = "my_monster_obj_key";
+
+    TextView monsterUrl;
+    View progressBar;
+
+    MonsterImageView monsterImageView_;
+    MonsterObject myMonsterObject;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monster_viewer);
+
+        MonsterPreferences monsterPreferences = MonsterPreferences.getInstance(getApplicationContext());
+        MonsterObject latestMonsterObj = monsterPreferences.getLatestMonsterObj();
+        Map<String, String> monsterMetaData = latestMonsterObj.monsterMetaData();
+
+        // Registers a custom event "monster_view" to track the user visited the monster view page
+        BranchEvent branchEvent = new BranchEvent("monster_view");
+        if (monsterMetaData != null) {
+            for (Map.Entry<String, String> entry : monsterMetaData.entrySet()) {
+                branchEvent.addCustomDataProperty(entry.getKey(), entry.getValue());
+            }
+        }
+        branchEvent.logEvent(getApplicationContext());
+
+        monsterImageView_ = (MonsterImageView) findViewById(R.id.monster_img_view);
+        monsterUrl = (TextView) findViewById(R.id.shareUrl);
+        progressBar = findViewById(R.id.progress_bar);
+
+        // Change monster
+        findViewById(R.id.cmdChange).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MonsterCreatorActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        // More info
+        findViewById(R.id.infoButton).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                InfoFragment infoFragment = InfoFragment.newInstance();
+                ft.replace(R.id.container, infoFragment).addToBackStack("info_container").commit();
+            }
+        });
+
+        //Share monster
+        findViewById(R.id.share_btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                shareMyMonster();
+            }
+        });
+
+        initUI();
+    }
+
+    private void initUI() {
+        myMonsterObject = getIntent().getParcelableExtra(MY_MONSTER_OBJ_KEY);
+        progressBar.setVisibility(View.VISIBLE);
+
+        if (myMonsterObject != null) {
+            String monsterName = myMonsterObject.getMonsterName();
+
+            // Create an object of LinkProperties to set the channel to be "sms"
+            LinkProperties linkProperties = new LinkProperties().setChannel("sms").setFeature("sharing");
+
+            // Create an object of ContentMetadata with the values from `myMonsterObject.prepareBranchDict()`
+            ContentMetadata contentMetadata = new ContentMetadata();
+            for (Map.Entry<String, String> entry : myMonsterObject.prepareBranchDict().entrySet()) {
+                contentMetadata.addCustomMetadata(entry.getKey(), entry.getValue());
+            }
+
+            // Create an object of BranchUniversalObject with the ContentMetadata object previously created
+            BranchUniversalObject branchUniversalObject = new BranchUniversalObject()
+                    .setTitle("Branchster Monster: " + myMonsterObject.getMonsterName())
+                    .setContentDescription("Monster created and shared by Branch's SDK")
+                    .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                    .setContentMetadata(contentMetadata);
+
+            // generate a shortURL and set the value of url text view.
+            branchUniversalObject.generateShortUrl(getApplicationContext(), linkProperties, new Branch.BranchLinkCreateListener() {
+                @Override
+                public void onLinkCreate(String url, BranchError error) {
+                    if (error == null) {
+                        monsterUrl.setText(url);
+                        new BranchEvent("branch_url_created").logEvent(getApplicationContext());
+                    } else {
+                        new BranchEvent("branch_error").logEvent(getApplicationContext());
+                    }
+                    progressBar.setVisibility(View.GONE);
+                }
+            });
+
+            // Set the value of monster name text view
+            if (!TextUtils.isEmpty(myMonsterObject.getMonsterName())) {
+                monsterName = myMonsterObject.getMonsterName();
+            }
+            ((TextView) findViewById(R.id.txtName)).setText(monsterName);
+
+            // Set the value of monster description text view
+            String description = MonsterPreferences.getInstance(this).getMonsterDescription();
+            if (!TextUtils.isEmpty(myMonsterObject.getMonsterDescription())) {
+                description = myMonsterObject.getMonsterDescription();
+            }
+            ((TextView) findViewById(R.id.txtDescription)).setText(description);
+
+            // set the image of monster image view
+            monsterImageView_.setMonster(myMonsterObject);
+        } else {
+            Log.e(TAG, "Monster is null. Unable to view monster");
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Method to share my custom monster with sharing with Branch Share sheet
+     */
+    private void shareMyMonster() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        // TODO: Replace with Branch-generated shortUrl
+
+        // Create an object of LinkProperties to set the channel to be "sms"
+        LinkProperties linkProperties = new LinkProperties()
+                .setChannel("sms")
+                .setFeature("sharing");
+//                .addControlParameter("$android_deeplink_path", "monster/view/");
+
+        // Create an object of ContentMetadata with the values from `myMonsterObject.prepareBranchDict()`
+        ContentMetadata contentMetadata = new ContentMetadata();
+        for (Map.Entry<String, String> entry : myMonsterObject.prepareBranchDict().entrySet()) {
+            contentMetadata.addCustomMetadata(entry.getKey(), entry.getValue());
+        }
+
+        // Create an object of BranchUniversalObject with the ContentMetadata object previously created
+        BranchUniversalObject branchUniversalObject = new BranchUniversalObject()
+                .setTitle("Branchster Monster: " + myMonsterObject.getMonsterName())
+                .setContentDescription("Monster created and shared by Branch's SDK")
+                .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+                .setContentMetadata(contentMetadata);
+
+        // generate a shortURL
+        branchUniversalObject.generateShortUrl(getApplicationContext(), linkProperties, new Branch.BranchLinkCreateListener() {
+            @Override
+            public void onLinkCreate(String url, BranchError error) {
+                if (error == null) {
+                    new BranchEvent("branch_url_created").logEvent(getApplicationContext());
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    i.putExtra(Intent.EXTRA_TEXT, String.format("Check out my Branchster named %s at %s", myMonsterObject.getMonsterName(), url));
+                    startActivityForResult(i, SEND_SMS);
+                } else {
+                    new BranchEvent("branch_error").logEvent(getApplicationContext());
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (SEND_SMS == requestCode) {
+            if (RESULT_OK == resultCode) {
+                // TODO: Track successful share via Branch.
+                new BranchEvent("branch_url_shared").logEvent(getApplicationContext());
+                Toast.makeText(getApplicationContext(), "Link shared", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        FragmentManager fm = getFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            fm.popBackStack();
+        } else {
+            new AlertDialog.Builder(this)
+                    .setTitle("Exit")
+                    .setMessage("Are you sure you want to exit?")
+                    .setNegativeButton(android.R.string.no, null)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    }).create().show();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        //no-op
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        initUI();
+    }
+
+}
